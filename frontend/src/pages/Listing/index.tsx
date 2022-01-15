db548258ecc2d8c54e8f469bf2a1cc01a36eb7ac
Matheus Konrath <mkonrath96@gmail.com>
2022-01-15 15:00:02 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,15 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 function Listing() {
+
+    axios.get(`${BASE_URL}/movies`)
+    .then(response =>{
+        console.log(response.data);
+    });
+
 
     return (
         <>
@@ -25,7 +33,7 @@ function Listing() {
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                    
+
                 </div>
 
             </div>
